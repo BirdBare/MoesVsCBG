31a9ec275269b6c1dbc3254143351c7c3697f0a8
Bradley Bare <BradleyBare@gmail.com>
2013-11-24 03:25:07 -0600

    change includes for product name change to struct

--- customer.h
+++ customer.h
@@ -52,7 +52,7 @@
 using namespace std;
 
 #include <string>
-#include "product.h"
+#include "struct.h"
 #include "function.hpp"
 #include <iostream>
 
@@ -86,5 +86,5 @@ class customer
   bool FillCustData(const string file);
   friend ostream& operator<<(ostream& os, const customer& source);
   friend ostream& operator<<(ostream& os, const business& source);
-};
+ };
 #endif
